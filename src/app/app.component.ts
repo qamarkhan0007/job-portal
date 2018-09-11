86758865e75bfcc28392dc860ea214b714eb391f
Qamar Khan <qamar.khan@sofocle.com>
2018-09-11 14:14:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import * as $ from 'jquery';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Look jQuery Animation working in action!';
+  flag = false;
+
+
+  ngOnInit() {
+    $(document).ready(function() {
+    $("#css3dimagePager li").click(function(){
+      var rotateY = ($(this).index() * -90);
+      $("#css3dimageslider ul").css({"-webkit-transform":"rotateY("+rotateY+"deg)", "-moz-transform":"rotateY("+rotateY+"deg)", "-ms-transform":"rotateY("+rotateY+"deg)", "transform":"rotateY("+rotateY+"deg)"});
+      $("#css3dimagePager li").removeClass("active");
+      $(this).addClass("active");
+    });
+    $("#css3dtransparency").click(function() {
+      $("#css3dimageslider").toggleClass("transparency");
+      $(this).toggleClass("active");
+    });
+    });
+  }
+  changeFlag(){
+    this.flag = this.flag ? false: true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatGridListModule,
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatRadioModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatChipsModule,
+  MatTooltipModule,
+  MatExpansionModule,
+  MatDialogModule,
+  MatAutocompleteModule,
+  MatTabsModule,
+  MatSlideToggleModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
